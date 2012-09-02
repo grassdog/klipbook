279a7d27dc7499da0d9eeeae70ca8ed1f870a894
Ray Grasso <ray.grasso@gmail.com>
2012-09-02 19:32:58 +0800

    Get scraper working

--- lib/klipbook/sources/amazon_site/book_scraper.rb
+++ lib/klipbook/sources/amazon_site/book_scraper.rb
@@ -3,54 +3,36 @@ require 'mechanize'
 module Klipbook::Sources
   module AmazonSite
     class BookScraper
-      def initialize(agent, message_stream=$stdout)
-        @agent = agent
-        @message_stream = message_stream
-      end
 
-      def scrape_books(page)
-        books = []
-
-        begin
-          books << scrape_book(page)
-        end while page = get_next_page(page)
-
-        books
+      def scrape_book(page)
+        page.search(".//div[@class='bookMain yourHighlightsHeader']").map { |element| build_book(page, element) }
       end
 
       private
 
-      def scrape_book(page)
-        @message_stream.puts 'Fetching book'
-        page.search(".//div[@class='bookMain yourHighlightsHeader']").map { |element| build_book(element) }
-      end
-
-      def scrape_highlights(page)
-        page.search(".//div[@class='highlightRow yourHighlight']").map { |element| build_clipping(element) }
-      end
-
-      def get_next_page(page)
-        ret = page.search(".//a[@id='nextBookLink']").first
-        if ret and ret.attribute("href")
-          @agent.get("https://kindle.amazon.com" + ret.attribute("href").value)
-        else
-          nil
-        end
-      end
-
-      def build_book(element)
+      def build_book(page, element)
         Klipbook::Book.new do |b|
           b.asin = element.attribute("id").value.gsub(/_[0-9]+$/, "")
           b.author = element.xpath("span[@class='author']").text.gsub("\n", "").gsub(" by ", "").strip
           b.title = element.xpath("span/a").text
           b.last_update = extract_last_update(element.xpath("div[@class='lastHighlighted']").text)
+          b.clippings = scrape_clippings(page)
         end
+      end
+
+      def extract_last_update(text)
+        text = text.gsub('Last annotated on ', '')
+        DateTime.parse(text)
+      end
+
+      def scrape_clippings(page)
+        page.search(".//div[@class='highlightRow yourHighlight']").map { |element| build_clipping(element) }
       end
 
       def build_clipping(element)
         Klipbook::Clipping.new do |c|
           c.annotation_id = element.xpath("form/input[@id='annotation_id']").attribute("value").value
-          c.asin = element.xpath("p/span[@class='hidden asin']").text
+          #c.asin = element.xpath("p/span[@class='hidden asin']").text
           c.text = element.xpath("span[@class='highlight']").text
           c.type = :highlight
 
@@ -61,11 +43,6 @@ module Klipbook::Sources
             c.location = $1.to_i
           end
         end
-      end
-
-      def extract_last_update(text)
-        text = text.gsub('Last annotated on ', '')
-        DateTime.parse(text)
       end
     end
   end
--- lib/klipbook/sources/amazon_site/scraper.rb
+++ lib/klipbook/sources/amazon_site/scraper.rb
@@ -4,13 +4,12 @@ module Klipbook::Sources
   module AmazonSite
     class Scraper
       def initialize(username, password,
-                     book_scraper=Klipbook::Sources::AmazonSite::BookScraper,
+                     book_scraper=Klipbook::Sources::AmazonSite::BookScraper.new,
                      message_stream=$stdout, max_books=100)
         @max_books = max_books
         @message_stream = message_stream
         @agent = Mechanize.new
-
-        @book_scraper = book_scraper.new(@agent)
+        @book_scraper = book_scraper
 
         page = @agent.get("https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F&pageId=amzn_kindle&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&openid.assoc_handle=amzn_kindle&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select")
         @login_form = page.form('signIn')
@@ -20,17 +19,36 @@ module Klipbook::Sources
       end
 
       def books
-        scrape_site
+        @message_stream.puts 'Logging into site'
+        signin_submission = @agent.submit(@login_form)
+        page = @agent.click(signin_submission.link_with(:text => /Your Highlights/))
+
+        scrape_books(page)
       end
 
       private
 
-      def scrape_site
-        @message_stream.puts 'Logging into site'
-        signin_submission = @agent.submit(@login_form)
-        highlights_page   = @agent.click(signin_submission.link_with(:text => /Your Highlights/))
+      def scrape_books(page)
+        books = []
+        @message_stream.print 'Fetching books '
 
-        @book_scraper.scrape_books(highlights_page)
+        begin
+          @message_stream.print '.'
+          books << @book_scraper.scrape_book(page)
+        end while page = get_next_page(page)
+
+        puts ' Done!'
+
+        books.flatten
+      end
+
+      def get_next_page(page)
+        ret = page.search(".//a[@id='nextBookLink']").first
+        if ret and ret.attribute("href")
+          @agent.get("https://kindle.amazon.com" + ret.attribute("href").value)
+        else
+          nil
+        end
       end
     end
   end

26c69d7d39290e91d1ab2ffb7c9e4d26dd996097
Ray Grasso <ray.grasso@gmail.com>
2012-09-05 19:23:49 +0800

    Refactor scraper

--- lib/klipbook/sources/amazon_site/scraper.rb
+++ lib/klipbook/sources/amazon_site/scraper.rb
@@ -10,12 +10,6 @@ module Klipbook::Sources
         @message_stream = message_stream
         @agent = Mechanize.new
         @book_scraper = book_scraper
-
-        page = @agent.get("https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F&pageId=amzn_kindle&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&openid.assoc_handle=amzn_kindle&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select")
-        @login_form = page.form('signIn')
-
-        @login_form.email = username
-        @login_form.password = password
       end
 
       def books
@@ -25,11 +19,24 @@ module Klipbook::Sources
       private
 
       def fetch_books
-        @message_stream.puts 'Logging into site'
-        signin_submission = @agent.submit(@login_form)
+        login_form = login
+
+        signin_submission = @agent.submit(login_form)
         page = @agent.click(signin_submission.link_with(:text => /Your Highlights/))
 
         scrape_books(page)
+      end
+
+      def login
+        @message_stream.puts 'Logging into site'
+
+        page = @agent.get("https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F&pageId=amzn_kindle&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&openid.assoc_handle=amzn_kindle&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select")
+        login_form = page.form('signIn')
+
+        login_form.email = username
+        login_form.password = password
+
+        login_form
       end
 
       def scrape_books(page)

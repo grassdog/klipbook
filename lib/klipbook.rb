3a11b60fc1e2eec8d5bdf748c9632180d9ca9d36
Ray Grasso <ray.grasso@gmail.com>
2013-02-10 13:30:44 +0800

    Put more things under sources

--- lib/klipbook.rb
+++ lib/klipbook.rb
@@ -6,20 +6,20 @@ require 'klipbook/commands/collate'
 require 'klipbook/commands/list_books'
 require 'klipbook/commands/pretty_print'
 
+require 'klipbook/sources/book_source'
+require 'klipbook/sources/book'
+require 'klipbook/sources/clipping'
+require 'klipbook/sources/invalid_source_error'
+
 require 'klipbook/sources/kindle_device/file_parser'
 require 'klipbook/sources/kindle_device/entry_parser'
 require 'klipbook/sources/kindle_device/entry'
 require 'klipbook/sources/kindle_device/file'
 
-require 'klipbook/sources/book_source'
 require 'klipbook/sources/amazon_site/site_scraper'
 require 'klipbook/sources/amazon_site/book_scraper'
 
-require 'klipbook/invalid_source_error'
 require 'klipbook/config'
-require 'klipbook/book'
-require 'klipbook/clipping'
-
 require 'klipbook/collator'
 require 'klipbook/printer'
 

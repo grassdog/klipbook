6ac78001aff8a6fe4b594ba73ac907a092612a46
Ray Grasso <ray.grasso@gmail.com>
2013-12-12 08:34:17 +0800

    Increase timeout for web site specs

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,10 @@ $LOAD_PATH << File.expand_path('../../../lib', __FILE__)
 require 'klipbook'
 require 'fileutils'
 
+Before do
+  @aruba_timeout_seconds = 40
+end
+
 Before('@slow') do
   @aruba_io_wait_seconds = 40
 end

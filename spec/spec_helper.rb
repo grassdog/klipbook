e51f49edb8884f15f21230bb831d8df842dbe63f
Ray Grasso <ray.grasso@gmail.com>
2013-12-30 22:33:31 +0800

    Don't refer to deprecated backtrace_clean_patterns in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,3 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 $LOAD_PATH << File.expand_path('../../../lib', __FILE__)
 require 'klipbook'
-
-RSpec.configure do |config|
-  config.mock_with :rr
-end
